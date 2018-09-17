2c33b512c8959413b3d49dd462ef9d09fe66a4d4
qsg_zhengyi <zhengyi@qingsongguan.com>
2018-09-17 15:35:16 +0800

    change gitignore

--- src/main/kotlin/com/buyaoshushu/action/ToolMenuAction.kt
+++ src/main/kotlin/com/buyaoshushu/action/ToolMenuAction.kt
@@ -7,15 +7,15 @@ import com.intellij.openapi.ui.Messages
 import org.bff.javampd.server.MPD
 
 
+class ToolMenuAction : AnAction() {
+    lateinit var mpd: MPD
 
-class ToolMenuAction: AnAction() {
-   lateinit var mpd:MPD
-    init{
-        var propertiesComponent= PropertiesComponent.getInstance()
-        val server=propertiesComponent.getValue("intellMPD.server","0.0.0.0")
-        val port=propertiesComponent.getInt("intellMPD.port",6600)
-        val password=propertiesComponent.getValue("intellMPD.password")
-        mpd= MPD.Builder()
+    init {
+        var propertiesComponent = PropertiesComponent.getInstance()
+        val server = propertiesComponent.getValue("intellMPD.server", "0.0.0.0")
+        val port = propertiesComponent.getInt("intellMPD.port", 6600)
+        val password = propertiesComponent.getValue("intellMPD.password")
+        mpd = MPD.Builder()
                 .server(server)
                 .port(port).password(password)
                 .build()
@@ -23,15 +23,22 @@ class ToolMenuAction: AnAction() {
 
 
     override fun actionPerformed(event: AnActionEvent?) {
-        if(event!!.presentation.text=="Play")
+        var propertiesComponent = PropertiesComponent.getInstance()
+        val server = propertiesComponent.getValue("intellMPD.server", "0.0.0.0")
+        val port = propertiesComponent.getInt("intellMPD.port", 6600)
+        val password = propertiesComponent.getValue("intellMPD.password")
+        if (!mpd.isConnected) {
+            mpd = MPD.Builder().server(server).port(port).password(password).build()
+        }
+        if (event!!.presentation.text == "Play")
             mpd.player.play()
-        if(event.presentation.text=="Pause")
+        if (event.presentation.text == "Pause")
             mpd.player.pause()
-        if(event.presentation.text=="Next")
+        if (event.presentation.text == "Next")
             mpd.player.playNext()
-        if(event.presentation.text=="Previous")
+        if (event.presentation.text == "Previous")
             mpd.player.playPrevious()
-        if(event.presentation.text=="Stop")
+        if (event.presentation.text == "Stop")
             mpd.player.stop()
     }
 }
