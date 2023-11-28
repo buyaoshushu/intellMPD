b2654d93d7c0034e1ac24b7adb9eed4ada5a9f15
郑祎 <zhengyi@hosecloud.com>
2023-11-28 11:00:54 +0800

    add git ignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,38 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.9.0"
-    id("org.jetbrains.kotlin.jvm") version "1.7.20"
+    id("org.jetbrains.intellij") version "1.16.0"
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
 }
 
 group = "com.buyaoshushu"
-version = "1.1-SNAPSHOT"
+version = "1.3-SNAPSHOT"
 
 repositories {
+    maven {
+        url=uri("https://maven.aliyun.com/repository/public")
+    }
+    maven {
+        url=uri("https://maven.aliyun.com/repository/central")
+    }
+    maven {
+        url=uri("https://maven.aliyun.com/repository/google")
+    }
+    maven {
+        url=uri("https://maven.aliyun.com/repository/jcenter")
+    }
+    maven {
+        url=uri("https://maven.aliyun.com/repository/spring")
+    }
+    maven {
+        url=uri("https://maven.aliyun.com/repository/spring-plugin")
+    }
     mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.3.3")
+    version.set("2023.2.4")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -41,7 +59,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("213")
-        untilBuild.set("223.*")
+        untilBuild.set("233.*")
     }
 
     signPlugin {
